05028d4f77397970db562f458b24da67485161c1
Kavindu <wkanjana1590@gmail.com>
2025-11-08 18:40:38 +0530

    second commit

---
+++ src/Student.java
@@ -0,0 +1,15 @@
+public class Student {
+    private int id;
+    private String name;
+    private int age;
+    private String courses;
+    private double marks;
+
+    public Student(int id,String name,int age,String courses,double marks){
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.courses = courses;
+        this.marks = marks;
+    }
+}
