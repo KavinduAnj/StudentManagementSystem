370cbb85ca6c5c7548abb78af8718786af07afff
K.A. Wijesekara <159270664+KavinduAnj@users.noreply.github.com>
2025-11-09 23:56:18 +0530

    Add getters and setters to Student class

--- src/Student.java
+++ src/Student.java
@@ -11,5 +11,37 @@ public class Student {
         this.age = age;
         this.courses = courses;
         this.marks = marks;
+         //Getters
+    public int getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public int getAge(){
+        return age;
+    }
+    public String getCourses(){
+        return courses;
+    }
+    public double getMarks(){
+        return marks;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setAge(int age){
+        this.age = age;
+    }
+    public void setCourses(String courses){
+        this.courses = courses;
+    }
+    public void setMarks(double marks){
+        this.marks = marks;
+    }
+    @Override
+    public String toString() {
+        return id + " | "+ name +" | "+age+" | "+courses+" | "+marks;
+    
     }
 }
