e2a126023039f98cb99f87b522b052a09d5d6d31
KavinduAnj <wkanjana1590@gmail.com>
2025-11-10 21:41:47 +0530

    user input

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StudentService service = new StudentService();
+        int choice;
 
+        do {
+            System.out.println("\n===== Student Management System =====");
+            System.out.println("1. Add Student");
+            System.out.println("2. View Students");
+            System.out.println("3. Search Student");
+            System.out.println("4. Update Student");
+            System.out.println("5. Delete Student");
+            System.out.println("6. Exit");
+            System.out.print("Enter your choice: ");
+            choice = sc.nextInt();
+
+
+
+        } while (choice != 6);
     }
 }
