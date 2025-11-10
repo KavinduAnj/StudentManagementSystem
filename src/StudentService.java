874d9f43184414fe90dad2dc63e9150697f1477b
KavinduAnj <wkanjana1590@gmail.com>
2025-11-11 00:48:03 +0530

    id generator

--- src/StudentService.java
+++ src/StudentService.java
@@ -1,2 +1,13 @@
+import java.util.*;
 public class StudentService {
+    private List<Student> students;
+
+    public StudentService() {
+        students = FileHandler.loadStudents(); // Load existing data
+    }
+
+    private int generateId() {
+        return students.isEmpty() ? 1 : students.get(students.size() - 1).getId() + 1;
+    }
+
 }
