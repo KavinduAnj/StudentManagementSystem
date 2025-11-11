7def2c639aaa3becb5c1b5ece554492481b68280
KavinduAnj <wkanjana1590@gmail.com>
2025-11-11 20:42:32 +0530

    add student function

--- src/StudentService.java
+++ src/StudentService.java
@@ -10,4 +10,22 @@ public class StudentService {
         return students.isEmpty() ? 1 : students.get(students.size() - 1).getId() + 1;
     }
 
+    public void addStudent(Scanner sc){
+        System.out.print("Enter name: ");
+        sc.nextLine();
+        String name = sc.nextLine();
+        System.out.print("Enter age: ");
+        int age = sc.nextInt();
+        System.out.print("Enter course: ");
+        sc.nextLine();
+        String course = sc.nextLine();
+        System.out.print("Enter marks: ");
+        double marks = sc.nextDouble();
+
+        Student s = new Student(generateId(), name, age, course, marks);
+        students.add(s);
+        FileHandler.saveStudents(students);
+        System.out.println("✅ Student added successfully!");
+    }
+
 }
